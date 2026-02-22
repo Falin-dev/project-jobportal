4b0ca9c48a13b8b65e0c257af820024027eebffd
Falin-dev <mounsandofalin.ai23@stellamaryscoe.edu.in>
2026-02-22 16:41:31 +0530

    basic_login test view function add

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterJob, RegisterSerializer
-
+from django.contrib.auth import User
 @api_view(['GET'])
 def hello_api(request):
     return Response({'messages':"Hello good morning!","IDEA":'Ignite','call':'attend'})
@@ -22,3 +22,15 @@ def register_job(request):
         serializer.save()
         return Response({"message":"User Registered Successfully"},status=status.HTTP_201_CREATED)
     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def basic_login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    try:
+        user = User.objects.get(username=username,password=password)
+        return Response({"message":"Login Success"},status=status.HTTP_200_OK)
+    except:
+        return Response({"message":"Invalid Credentials"},status=status.HTTP_400_BAD_REQUEST)
